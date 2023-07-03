423c0d21e3ac23690e0d86bcfa5df7c2d7cb80f9
Eben <maradebenezer@gmail.com>
2023-07-04 00:32:07 +0100

    added 2

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,8 @@
+# automate creating custom header
+exec { 'command':
+	command => 'apt-get -y update;
+	apt-get -y install nginx;
+	sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;
+	service nginx restart',
+	provider => shell,
+}
