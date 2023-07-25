3225a6807f62d53d198549ad1634b7d860298985
Eben <maradebenezer@gmail.com>
2023-07-25 04:08:44 +0100

    update

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,44 +1,24 @@
 #!/usr/bin/python3
 """
-get data from API, retrieving TODO list data
+get data from API, retrieving TODO list
 given employee ID
-export result to CSV
+export data to csv
 """
 
 import csv
 import requests
-from sys import argv
+import sys
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-    # get user passed in as argument
-    id = int(argv[1])
-
-    # retrieve user data
-    user_url = "https://jsonplaceholder.typicode.com/users"
-    user_resp = requests.get(user_url)
-    user_json = user_resp.json()
-    user_dict = {}
-    for user in user_json:
-        user_dict[user['id']] = user.get('username')
-
-    # retrieve todo data
-    todo_url = "https://jsonplaceholder.typicode.com/todos"
-    todo_resp = requests.get(todo_url)
-    todo_json = todo_resp.json()
-
-    # parse data
-    name = user_dict[id]
-    all_rows = []
-    for todo in todo_json:
-        if todo.get('userId') == id:
-            all_rows.append([str(id), name,
-                            todo.get('completed'), todo.get('title')])
-            if todo.get('userId') != id:
-                break
-
-    # write to csv
-    with open('USER_ID.csv', 'w', newline='') as file:
-        writer = csv.writer(file, delimiter=',', quoting=csv.QUOTE_ALL)
-        writer.writerows(all_rows)
+    with open("USER_ID.csv", "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        [writer.writerow(
+            [user_id, username, t.get("completed"), t.get("title")]
+         ) for t in todos]
--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,49 +1,25 @@
 #!/usr/bin/python3
 """
-get data from API, retrieving TODO list data
+get data from API, retrieving TODO list
 given employee ID
-export result to JSON
+export data to JSON
 """
 
 import json
 import requests
-from sys import argv
+import sys
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-    # get user passed in as argument
-    id = int(argv[1])
-
-    # retrieve user data
-    user_url = "https://jsonplaceholder.typicode.com/users"
-    user_resp = requests.get(user_url)
-    user_json = user_resp.json()
-    user_dict = {}
-    for user in user_json:
-        user_dict[user['id']] = user.get('username')
-
-    # retrieve todo data
-    todo_url = "https://jsonplaceholder.typicode.com/todos"
-    todo_resp = requests.get(todo_url)
-    todo_json = todo_resp.json()
-
-    # parse data
-    name = user_dict[id]
-    all_tasks = []
-    data = {str(id): all_tasks}
-    for todo in todo_json:
-        if todo.get('userId') == id:
-            task_dict = {
-                "task": todo.get('title'),
-                "completed": todo.get('completed'),
-                "username": name
-            }
-            all_tasks.append(task_dict)
-
-            if todo.get('userId') != id:
-                break
-
-    # write to csv
-    with open('USER_ID.json', 'w') as file:
-        json.dump(data, file)
+    with open("USER_ID.json".format(user_id), "w") as jsonfile:
+        json.dump({user_id: [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": username
+            } for t in todos]}, jsonfile)
