c52f22a00162f1ca2d884a5145e57e797865bfd0
Eben <maradebenezer@gmail.com>
2023-08-15 03:39:54 +0100

    fixed puppet

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,8 +2,8 @@
 
 exec { 'fix--for-nginx':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => '/usr/local/bin/:/bin/'
-} ->
+  path    => '/usr/local/bin/:/bin/',
+}
 
 # Restart Nginx
 exec { 'nginx-restart':
